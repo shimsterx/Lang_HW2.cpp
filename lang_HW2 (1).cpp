1206e082133b8ce6baff73a59ea969fa606bc8f0
shimsterx <157174526+shimsterx@users.noreply.github.com>
2024-03-18 17:19:01 -0500

    Add files via upload

---
+++ lang_HW2 (1).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Display the name in a box of stars
+    cout << "*****************" << endl;
+    cout << "* Willem Lang      *" << endl;
+    cout << "*****************" << endl;
+
+    return 0;
+}
+
